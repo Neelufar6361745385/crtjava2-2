332fe26230b2ffeecbe2265090594491639295d3
Neelufar6361745385 <155129035+Neelufar6361745385@users.noreply.github.com>
2023-12-29 14:45:59 +0530

    Add files via upload

---
+++ evenoddd.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class Main 
+{
+  
+public static void main (String[]args)
+  {
+    
+int n;
+  
+
+Scanner s = new Scanner (System.in);
+  
+System.out.println ("enter a number");
+  
+n = s.nextInt ();
+  
+ 
+if (n % 2 == 0)
+    
+    {
+      
+System.out.println ("even");
+    
+}
+  
+  else
+    
+System.out.println ("odd");
+
+ 
+}
+
+}
+
+
