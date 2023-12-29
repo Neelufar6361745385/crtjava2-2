da163e272a7e595d45cbccb8a803ec50454b708e
Neelufar6361745385 <155129035+Neelufar6361745385@users.noreply.github.com>
2023-12-29 15:15:32 +0530

    Add files via upload

---
+++ positive or negative number.java
@@ -0,0 +1,19 @@
+ import java.util.*;
+class Main
+{
+	public static void main(String[] args) {
+	int a;
+	Scanner s = new Scanner (System.in);
+	System.out.println("enter a num");
+	a=s.nextInt();
+	if(a>0)
+{
+		System.out.println("positive number ");
+}
+else if(a<0)
+{	System.out.println("nagetive number");
+}
+else
+{	
+	System.out.println("zero");
+}}}
