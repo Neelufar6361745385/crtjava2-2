342703ddad4801704f4936d4be182d465bfdd3c3
Neelufar6361745385 <155129035+Neelufar6361745385@users.noreply.github.com>
2023-12-30 11:32:32 +0530

    Add files via upload

---
+++ three nos ternary.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Main
+
+{
+   
+ public static void main(String[]args)
+
+    {
+     
+   Scanner s=new Scanner(System.in);
+ 
+
+       int a,b,c;
+ 
+       a=s.nextInt();
+ 
+       b=s.nextInt();
+ 
+       c=s.nextInt();
+ 
+       int r=(a<b&&a<c)?a:(b<c)?b:c;
+ 
+       System.out.println(r);
+   
+ }
+   
+ 
+
+}
