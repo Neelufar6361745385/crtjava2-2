cf1083add3835da3975d8718e2705a23c0fd01af
Neelufar6361745385 <155129035+Neelufar6361745385@users.noreply.github.com>
2024-01-11 15:48:57 +0530

    Add files via upload

---
+++ Sorting.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Sorting
+{
+	public static void main(String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		int a[],i,n;
+		System.out.println("Enter size of the array");
+		n=s.nextInt();
+		a=new int[n];
+		System.out.println(" Enter n elements");
+		for(i=0;i<n;i++)
+		{
+			System.out.println(a[i]+"  ");
+		}
+		System.out.println(");
+		Arrays.sort(a);
+		System.out.println("The Elements after Sorting");
+		for(i=0;i<n;i++)
+		{
+			System.out.print(a[i]+"  ");
+		}
+	}
+}
+
+	
