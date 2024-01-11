9dddaac72c63f5e6ff22195b2b6de679c437e341
Neelufar6361745385 <155129035+Neelufar6361745385@users.noreply.github.com>
2024-01-11 15:46:51 +0530

    Add files via upload

--- Tebd.java
+++ Tebd.java
@@ -1,17 +1,16 @@
 import java.util.*;
 class Tebd
 {
-	public static void main(String[] args)
+	public static void main(String args[])
 	{
 		Scanner s=new Scanner(System.in);
 		int a,b,r;
-		System.out.println("Enter two number");
+		System.out.println("ENTER TWO NUMBERS");
 		a=s.nextInt();
 		b=s.nextInt();
 		r=a/b;
-		System.out.println("Divistion="+r);
+		System.out.println("DIVISION="+r);
 	}
 }
 
-
- 
+		
