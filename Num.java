05d516f07a8407dc22ab2693ad8834a4e88d5c75
Neelufar6361745385 <155129035+Neelufar6361745385@users.noreply.github.com>
2024-01-11 15:50:54 +0530

    Add files via upload

---
+++ Num.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class Num
+{
+	public static void main(String[] args)
+	{
+		Scanner s=new Scanner(System.in);
+		int a;
+		System.out.println("Enter a Number");
+		a=s.nextInt();
+		if(a>0)
+		{
+			System.out.println("The given number is positive number");
+		}
+		else
+		if(a<0)
+		{
+			System.out.println("The given number is Negative Number");
+		}
+		else
+		{
+			System.out.println("the given Number is Zero");
+		}
+		
+
+	}
+}
+
+
+		
+
+		
+
+
+
+
