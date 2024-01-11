46eef3d9b56b2c04bfc0ee99aae8b84b06c6c782
Neelufar6361745385 <155129035+Neelufar6361745385@users.noreply.github.com>
2024-01-11 15:48:36 +0530

    Add files via upload

---
+++ Students.java
@@ -0,0 +1,27 @@
+class Student
+{
+	int admno;
+	String name;
+	void get(int admno,String name)
+	{
+		this.admno=admno;
+		this.name=name;
+	}
+	void show()
+	{
+		System.out.println("ADMISSION NO="+admno);
+		System.out.println("NAME="+name);
+	}
+}
+class Marks extends Student
+{
+	int mat,phy,che;
+	void get
+	{
+		
+	}
+{
+}
+} 
+{
+	
